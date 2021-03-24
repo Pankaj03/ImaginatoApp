6d8f949e51ce6764071a831e5ef9a6bd19fcdc58
VishalNeha <gargneha2310@gmail.com>
2021-03-24 14:36:21 +0530

    Project setup

---
+++ ImaginatoApp/ImaginatoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImaginatoApp
+//
+//  Created by VishalNeha on 24/03/21.
+//  Copyright © 2021 Pankaj Asudani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImaginatoApp/Pods/Target Support Files/Pods-ImaginatoApp/Pods-ImaginatoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImaginatoApp : NSObject
+@end
+@implementation PodsDummy_Pods_ImaginatoApp
+@end
