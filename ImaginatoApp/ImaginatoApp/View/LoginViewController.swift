f94d83c28a901c69fe274487a923ffbe42202e3b
VishalNeha <gargneha2310@gmail.com>
2021-03-24 15:02:09 +0530

    Create UI for login screen

--- ImaginatoApp/ImaginatoApp/View/LoginViewController.swift
+++ ImaginatoApp/ImaginatoApp/View/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  ImaginatoApp
 //
 //  Created by VishalNeha on 24/03/21.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.largeTitleDisplayMode = .always
+    }
 
 }
 
